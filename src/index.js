3d71648e628405515486004280b08a246b089f87
Mohebul <emonm1@roehampton.ac.uk>
2024-03-21 13:40:27 +0000

    fils uploded

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Import dependencies
+const express = require("express");
+const mysql = require("mysql2");
+const countryRoutes = require("./models/country");
+
+// Create express instance
+const app = express();
+const port = 3000;
+
+// Setup database connection
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST || "localhost",
+  user: "user",
+  password: "password",
+  database: "world",
+});
+
+// Setup middleware, view engine, etc.
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('static'));
+
+// Use the routes defined in countryRoutes.js, passing the app and db as arguments
+countryRoutes(app, db);
+
+// Home Page route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Run server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
